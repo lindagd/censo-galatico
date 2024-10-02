6687c2896efba20f2e9ce6b786f09ee9897cb0e7
lindagd <liwdag@gmail.com>
2024-10-02 10:19:32 -0300

    Tarefa 4: implementar busca por planetas

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const search_input = document.getElementById('search-bar');
+const planet_div = document.querySelector('.planet-info');
+
+search_input.addEventListener('input', function() {
+  const query = search_input.value.trim();
+  if (query) {
+    searchPlanet(query);
+  } else {
+    planet_div.innerHTML = '';
+  }
+});
+
 async function fetchPlanets() {
   let response = await fetch('https://swapi.dev/api/planets/?format=json');
   const {results} = await response.json();
@@ -15,14 +27,21 @@ async function fetchPlanets() {
   });
 }
 
-function showPlanet(planet) {
-  let planet_div = document.querySelector('.planet-info');
+async function searchPlanet(input) {
+  let response = await fetch(`https://swapi.dev/api/planets/?search=${input}`);
+  const {results} = await response.json();
+  
+  if (results.length > 0) {
+    return showPlanet(results[0])
+  }
+  planet_div.innerHTML = '<div class="search-not-found">Nenhum planeta encontrado :(</div>';
+}
 
+function showPlanet(planet) {
   planet_div.innerHTML = `<h2>${planet.name}</h2>
                           <p><strong>Clima:</strong> ${planet.climate}</p>
                           <p><strong>População:</strong> ${planet.population}</p>
                           <p><strong>Terreno:</strong> ${planet.terrain}</p>`;
 }
-
 
 fetchPlanets();
