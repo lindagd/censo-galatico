be3b4422a0d24c274a85bfe2e3281939ab8a43d8
lindagd <liwdag@gmail.com>
2024-10-01 21:27:52 -0300

    Tarefa 2: listar planetas e exibir em botões

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ async function fetchPlanets() {
   let response = await fetch('https://swapi.dev/api/planets/?format=json');
   const {results} = await response.json();
 
-  console.log(results);
+  let planet_list = document.querySelector('.planets-container');
+  results.forEach(planet => {
+    planet_button = document.createElement('button');
+    planet_button.innerHTML = planet.name;
+
+    planet_list.appendChild(planet_button);
+  });
 }
 
 fetchPlanets();
