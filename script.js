964e792840482b54feaf26f61a1ccf5fa13406d8
lindagd <liwdag@gmail.com>
2024-10-01 22:07:03 -0300

    Tarefa 3: consultar planeta e exibir na tela

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ async function fetchPlanets() {
     planet_button = document.createElement('button');
     planet_button.innerHTML = planet.name;
 
+    planet_button.addEventListener('click', () => {
+      showPlanet(planet);
+    });
+
     planet_list.appendChild(planet_button);
   });
 }
+
+function showPlanet(planet) {
+  let planet_div = document.querySelector('.planet-info');
+
+  planet_div.innerHTML = `<h2>${planet.name}</h2>
+                          <p><strong>Clima:</strong> ${planet.climate}</p>
+                          <p><strong>População:</strong> ${planet.population}</p>
+                          <p><strong>Terreno:</strong> ${planet.terrain}</p>`;
+}
+
 
 fetchPlanets();
