4f715bfd486107b7d3423bb6f4bd136bc64ec03b
lindagd <liwdag@gmail.com>
2024-10-02 14:07:30 -0300

    Tarefa Bônus: exibir habitantes populares de um planeta

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const search_input = document.getElementById('search-bar');
-const planet_div = document.querySelector('.planet-info');
+const planet_div = document.querySelector('.about-planet');
+const planet_list = document.querySelector('.planets-container');
+const residentsTable = document.querySelector('.about-residents');
 
 search_input.addEventListener('input', function() {
   const query = search_input.value.trim();
@@ -7,14 +9,14 @@ search_input.addEventListener('input', function() {
     searchPlanet(query);
   } else {
     planet_div.innerHTML = '';
+    residentsTable.innerHTML = '';
   }
 });
 
 async function fetchPlanets() {
-  let response = await fetch('https://swapi.dev/api/planets/?format=json');
+  const response = await fetch('https://swapi.dev/api/planets/?format=json');
   const {results} = await response.json();
 
-  let planet_list = document.querySelector('.planets-container');
   results.forEach(planet => {
     planet_button = document.createElement('button');
     planet_button.innerHTML = planet.name;
@@ -28,20 +30,48 @@ async function fetchPlanets() {
 }
 
 async function searchPlanet(input) {
-  let response = await fetch(`https://swapi.dev/api/planets/?search=${input}`);
+  const response = await fetch(`https://swapi.dev/api/planets/?search=${input}`);
   const {results} = await response.json();
   
   if (results.length > 0) {
     return showPlanet(results[0])
   }
   planet_div.innerHTML = '<div class="search-not-found">Nenhum planeta encontrado :(</div>';
+  residentsTable.innerHTML = '';
+}
+
+function fetchResidents(residents) {
+  residentsTable.innerHTML = '';
+
+  if (residents.length == 0) {
+    residentsTable.innerHTML = '<p>Não há informações sobre seus habitantes.</p>';
+    return;
+  }
+  
+  const table = document.createElement('table');
+  table.innerHTML = `<tr>
+                      <th>Nome</th>
+                      <th>Nascimento</th>
+                    </tr>`;
+  residentsTable.appendChild(table);
+
+  residents.forEach(async url => {
+    const res = await fetch(url);
+    const resident = await res.json();
+
+    const new_row = document.createElement('tr');
+    new_row.innerHTML = `<td>${resident.name}</td><td>${resident.birth_year}</td>`;
+    table.appendChild(new_row);
+  });
 }
 
 function showPlanet(planet) {
-  planet_div.innerHTML = `<h2>${planet.name}</h2>
+  planet_div.innerHTML = `<div class="about-planet"><h2>${planet.name}</h2>
                           <p><strong>Clima:</strong> ${planet.climate}</p>
                           <p><strong>População:</strong> ${planet.population}</p>
-                          <p><strong>Terreno:</strong> ${planet.terrain}</p>`;
+                          <p><strong>Terreno:</strong> ${planet.terrain}</p></div>`;
+
+  fetchResidents(planet.residents);
 }
 
 fetchPlanets();
